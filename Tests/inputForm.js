81f9c8476d42f5437efa4ebeb4d9f4351d91208d
Sherin Elmoghazy <isiselmoghazy@gmail.com>
2024-09-09 20:55:55 +0200

    test

---
+++ Tests/inputForm.js
@@ -0,0 +1,22 @@
+const {By, Builder} = require('selenium-webdriver');
+
+async function inputForm (params) {
+
+    let driver;
+    try{
+        driver = await new Builder().forBrowser('firefox').build();
+        await driver.get('https://demoqa.com/text-box');
+        await driver.findElement(By.id("userName")).sendKeys("My First Name");
+        await driver.findElement(By.id("userEmail")).sendKeys("email@gmail.com");
+        await driver.findElement(By.id("currentAddress")).sendKeys("Berlin, PBerg");
+        await driver.findElement(By.id("permanentAddress")).sendKeys("Berlin Kreuzberg");
+        await driver.findElement(By.id("submit")).submit();
+    } catch (e) {
+        console.log(e)
+    }finally{
+       // await driver.quit();
+    }
+    
+}
+
+inputForm()
