ce74ef58064b8b70b745d8ef51a70d924ff9b095
Yasmin elkoshy <yasmin.elkoshy@gmail.com>
2024-10-07 20:00:25 +0200

    merge

--- Tests/FirstTest.js
+++ Tests/FirstTest.js
@@ -1,12 +1,12 @@
 const {By, Builder} = require('selenium-webdriver');
 
-async function firstTest(params) {
+async function firstTest() {
 
     let driver;
     try{
         driver = await new Builder().forBrowser('firefox').build();
         await driver.get('https://www.google.com/');
-
+        
     }catch(e){
         console.log(e)
     }finally{
