1d4d1bfa45b9bca4d728665fa1673cc7f5ece99c
fatou <fatou.bernon@gmail.com>
2024-10-19 14:52:04 +0200

    Corrected "key" Lines

--- Tests/Practice Form - Fatou.js
+++ Tests/Practice Form - Fatou.js
@@ -1,20 +1,24 @@
-const {By, Builder} = require('selenium-webdriver');
+const {By, Builder, Key} = require('selenium-webdriver');
 const firefox = require('selenium-webdriver/firefox');
 async function PracticeForm() {
 
     let driver;
     try{
         driver = await new Builder().forBrowser('firefox').build();
-        let url = 'https://demoqa.com/automation-practice-form'
-        await driver.get(url);
+        await driver.get('https://demoqa.com/automation-practice-form');
+        await driver.manage().window().maximize();
         
+        //Scroll down to see the elements
+        await driver.executeScript("window.scrollTo(0, document.body.scrollHeight/3);");
+        await driver.sleep(3000);
+
         // Fill out the fields
         await driver.findElement(By.id("firstName")).sendKeys("Sophie");
         await driver.findElement(By.id("lastName")).sendKeys("Juillet");
         await driver.findElement(By.id("userEmail")).sendKeys("test@gmail.com");
 
         // Select gender radio button
-        await driver.findElement(By.id("gender-radio-2")).click();
+        await driver.findElement(By.cssSelector("label[for='gender-radio-2']")).click();
 
         await driver.findElement(By.id("userNumber")).sendKeys("0706151825");
         
@@ -26,13 +30,21 @@ async function PracticeForm() {
         await driver.findElement(By.cssSelector("option[value='2001']")).click();// Select a year
         await driver.findElement(By.cssSelector(".react-datepicker__day--001")).click();// Select day
         
-        await driver.findElement(By.id("subjectsContainer")).sendKeys('E');
-        await driver.findElement(By.id("hobbies-checkbox-2")).click();
+        //subject input
+        let input = await driver.findElement(By.xpath("//input[@id='subjectsInput']"))
+        await input.sendKeys('a');
+        await input.sendKeys(Key.ARROW_DOWN);
+        await input.sendKeys(Key.ENTER);
 
+        //upload file
+        let file = path.resolve("C:\\Users\\fatou\\Desktop\\Pour le test")
+        await driver.findElement(By.id('uploadPicture')).sendKeys(file)
+       
+        //Select state and city
         await driver.findElement(By.id("currentAddress")).sendKeys("Somewhere in India");
         await driver.findElement(By.cssSelector('#state .css-1hwfws3')).click();
-        await driver.findElement(By.cssSelector("div[id^='react-select-3-option-0']")).click();//Open dropdown and select state
-        await driver.findElement(By.cssSelector('#city .css-1hwfws3')).click();//Open dropdown an select a city
+        await driver.findElement(By.cssSelector("div[id^='react-select-3-option-0']")).click();
+        await driver.findElement(By.cssSelector('#city .css-1hwfws3')).click();
         await driver.findElement(By.cssSelector("div[id^='react-select-4-option-0']")).click();
         await driver.findElement(By.id('submit')).click();//Click on submit button
     }catch(e){
