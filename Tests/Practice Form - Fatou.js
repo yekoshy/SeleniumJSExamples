1a62c1150492afa4a2fd127d133adcabc63bbe68
fatou <fatou.bernon@gmail.com>
2024-10-19 15:09:19 +0200

    "Added const path to fixe ReferenceError"

--- Tests/Practice Form - Fatou.js
+++ Tests/Practice Form - Fatou.js
@@ -1,5 +1,6 @@
 const {By, Builder, Key} = require('selenium-webdriver');
 const firefox = require('selenium-webdriver/firefox');
+const = require('path');
 async function PracticeForm() {
 
     let driver;
@@ -18,17 +19,17 @@ async function PracticeForm() {
         await driver.findElement(By.id("userEmail")).sendKeys("test@gmail.com");
 
         // Select gender radio button
-        await driver.findElement(By.cssSelector("label[for='gender-radio-2']")).click();
+        await driver.findElement(By.css("label[for='gender-radio-2']")).click();
 
         await driver.findElement(By.id("userNumber")).sendKeys("0706151825");
         
         //Open dropdown and select Date of birth
         await driver.findElement(By.id("dateOfBirthInput")).click();
         await driver.findElement(By.className("react-datepicker__month-select")).click(); // Open month dropdown
-        await driver.findElement(By.cssSelector("option[value='0']")).click(); // Select January
+        await driver.findElement(By.css("option[value='0']")).click(); // Select January
         await driver.findElement(By.className("react-datepicker__year-select")).click();// Open year dropdown
-        await driver.findElement(By.cssSelector("option[value='2001']")).click();// Select a year
-        await driver.findElement(By.cssSelector(".react-datepicker__day--001")).click();// Select day
+        await driver.findElement(By.css("option[value='2001']")).click();// Select a year
+        await driver.findElement(By.css(".react-datepicker__day--001")).click();// Select day
         
         //subject input
         let input = await driver.findElement(By.xpath("//input[@id='subjectsInput']"))
@@ -42,10 +43,10 @@ async function PracticeForm() {
        
         //Select state and city
         await driver.findElement(By.id("currentAddress")).sendKeys("Somewhere in India");
-        await driver.findElement(By.cssSelector('#state .css-1hwfws3')).click();
-        await driver.findElement(By.cssSelector("div[id^='react-select-3-option-0']")).click();
-        await driver.findElement(By.cssSelector('#city .css-1hwfws3')).click();
-        await driver.findElement(By.cssSelector("div[id^='react-select-4-option-0']")).click();
+        await driver.findElement(By.css('#state .css-1hwfws3')).click();
+        await driver.findElement(By.css("div[id^='react-select-3-option-0']")).click();
+        await driver.findElement(By.css('#city .css-1hwfws3')).click();
+        await driver.findElement(By.css("div[id^='react-select-4-option-0']")).click();
         await driver.findElement(By.id('submit')).click();//Click on submit button
     }catch(e){
         console.log(e)
